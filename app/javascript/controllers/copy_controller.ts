2610fee08e00c97433eed7312e7a10d575e5875c
Caleb Denio <cjdenio44@gmail.com>
2021-11-26 23:22:08 -0500

    feat: Improve clipboard controller

--- app/javascript/controllers/copy_controller.ts
+++ app/javascript/controllers/copy_controller.ts
@@ -4,8 +4,25 @@ export default class extends Controller {
   static values = {
     text: String,
   };
+  static targets = ["button"];
+
+  timeout: NodeJS.Timeout | null;
+
+  initialize() {
+    this.timeout = null;
+  }
+
+  disconnect() {
+    clearTimeout(this.timeout);
+  }
 
   copy() {
     navigator.clipboard.writeText(this.textValue);
+
+    this.buttonTarget.innerText = "Copied!";
+
+    this.timeout = setTimeout(() => {
+      this.buttonTarget.innerText = "Copy";
+    }, 2000);
   }
 }
