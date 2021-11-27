d51e6c29e271c6efec80985d01c1d31f54a68ecf
Caleb Denio <cjdenio44@gmail.com>
2021-11-26 23:48:59 -0500

    feat: Somewhat useful settings page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
   def gravatar(email, size = 50)
     gravatar_id = Digest::MD5.hexdigest(email)
     gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}?d=mp&s=#{size}"
-    image_tag(gravatar_url, class: 'rounded-full', title: email)
+    image_tag(gravatar_url, class: 'rounded-full', title: email, alt: email)
   end
 end
