a616d116522a984a5a9143c11867cce8e2d83d2d
Caleb Denio <cjdenio44@gmail.com>
2021-12-02 10:39:06 -0500

    feat: show icon next to session

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module SettingsHelper
+	def browser_to_icon(browser)
+		if browser.chrome?
+			'chrome'
+		elsif browser.safari?
+			'safari'
+		elsif browser.firefox?
+			'firefox-browser'
+		elsif browser.edge?
+			'edge'
+		else
+			'window-maximize'
+		end
+	end
 end
