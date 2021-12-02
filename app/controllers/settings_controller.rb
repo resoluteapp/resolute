28d529d293f7611c5d519508765024a69b3bd766
Caleb Denio <cjdenio44@gmail.com>
2021-12-02 08:11:34 -0500

    fix: important bug

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -11,6 +11,7 @@ class SettingsController < ApplicationController
 		# TODO: limit
 		@sessions = @current_user.sessions.order(created_at: :desc)
 		@apps = ApiToken.select('oauth_app_id, MAX(created_at) AS last_authorized_at')
+																		.where(user_id: @current_user.id)
 																		.group('oauth_app_id')
 																		.order(last_authorized_at: :desc)
 	end
