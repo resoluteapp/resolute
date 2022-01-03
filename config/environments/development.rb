cd8f406db57bd21d03474493cfb209ac9ffda503
Caleb Denio <cjdenio44@gmail.com>
2022-01-02 22:54:24 -0500

    feat: Make landing demo responsive

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+	config.hosts << 'resolute-app.ngrok.io'
+
 	# Settings specified here will take precedence over those in config/application.rb.
 
 	# In the development environment your application's code is reloaded any time
