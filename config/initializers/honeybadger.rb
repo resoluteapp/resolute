9a0842207800fd71de2fa255fabf65d05eb2bd9b
Caleb Denio <cjdenio44@gmail.com>
2022-02-07 23:02:54 -0500

    fix: Report proper backtrace to Honeybadger

---
+++ config/initializers/honeybadger.rb
@@ -0,0 +1,12 @@
+Honeybadger.configure do |config|
+	config.before_notify do |notice|
+		# Change "errors" to match your custom controller name.
+		break if notice.component != 'errors'
+
+		# Look up original route path and override controller/action
+		# in Honeybadger.
+		params = Rails.application.routes.recognize_path(notice.url)
+		notice.component = params[:controller]
+		notice.action = params[:action]
+	end
+end
