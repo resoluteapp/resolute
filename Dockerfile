bf187fff6c4cf81257a808e24138a84e484a6a7a
Caleb Denio <cjdenio44@gmail.com>
2022-03-18 12:57:32 -0400

    feat(error-reporting): Report frontend exceptions to Honeybadger

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ruby:3.0.3
 
+ARG HONEYBADGER_FRONTEND_API_KEY
+
 ENV RAILS_ENV production
 ENV NODE_ENV production
 
@@ -9,12 +11,9 @@ RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && \
     apt-get install -y nodejs && \
     corepack enable
 
-COPY Gemfile Gemfile.lock ./
-
-RUN bundle install
-
 COPY . .
 
-RUN SECRET_KEY_BASE=1 bin/rails assets:precompile
+RUN bundle install && \
+    SECRET_KEY_BASE=1 bin/rails assets:precompile
 
 CMD ["bin/rails", "server"]
--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,19 +1,43 @@
+declare global {
+	interface Window {
+		Stimulus: Application;
+		User: {
+			user_id?: number;
+			user_email?: string;
+		};
+	}
+
+	// Variables provided by bin/esbuild
+	const HONEYBADGER_API_KEY: string | undefined;
+	const NODE_ENV: string | undefined;
+	const GIT_REVISION: string | undefined;
+}
+
 import "@hotwired/turbo-rails";
 import { Application } from "@hotwired/stimulus";
 import controllers from "./controllers";
+
+import Honeybadger from "@honeybadger-io/js/dist/browser/honeybadger";
+
+Honeybadger.configure({
+	apiKey: HONEYBADGER_API_KEY,
+	environment: NODE_ENV ?? "development",
+	revision: GIT_REVISION,
+});
+
+Honeybadger.setContext(window.User);
 
 import Alpine from "alpinejs";
 import persist from "@alpinejs/persist";
 import collapse from "@alpinejs/collapse";
 
-declare global {
-	interface Window {
-		Stimulus: Application;
-	}
-}
-
 const application = Application.start();
 window.Stimulus = application;
+
+application.handleError = (error, message, detail) => {
+	console.warn(message, detail);
+	Honeybadger.notify(error);
+};
 
 Alpine.plugin(persist);
 Alpine.plugin(collapse);
