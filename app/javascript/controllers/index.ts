0f8cb8c2d23197a4a49e8d3e697f8ac2637466bb
Caleb Denio <cjdenio44@gmail.com>
2022-10-05 21:54:09 -0400

    upgrades and refactor and stuff

--- app/javascript/controllers/index.ts
+++ app/javascript/controllers/index.ts
@@ -4,12 +4,12 @@ import AlertController from "./alert_controller";
 import CopyController from "./copy_controller";
 import FormConfirmController from "./form_confirm_controller";
 import HovercardController from "./hovercard_controller";
-import ResetFormController from "./reset_form_controller";
+import FormController from "./form_controller";
 
 export default (application: Application) => {
 	application.register("alert", AlertController);
 	application.register("copy", CopyController);
 	application.register("form-confirm", FormConfirmController);
 	application.register("hovercard", HovercardController);
-	application.register("reset-form", ResetFormController);
+	application.register("form", FormController);
 };
